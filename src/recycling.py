d8770c496743527c235a05b7cfddcbf93ba51ef5
Artur Vidal Krause <avidalkrause@gmail.com>
2025-09-01 01:12:55 -0300

    Plotter.py implemented

--- src/recycling.py
+++ src/recycling.py
@@ -4,6 +4,7 @@ from enums import *
 from rewarder import Rewarder
 from state_updater import StateUpdater
 from robot import Robot
+import os
 
 class Recycling:
     def __init__(self, state_updater, robot, num_runs_per_epoch=1000) -> None:
@@ -11,13 +12,17 @@ class Recycling:
         self.robot = robot
         self.training_counter = 0
         self.__num_runs_per_epoch = num_runs_per_epoch
+        self.data_dir = "data" # Directory data
 
-        with open("rewards.txt", "a") as f:
+        os.makedirs(self.data_dir, exist_ok=True)
+        self.rewards_path = os.path.join(self.data_dir, "rewards.txt")
+        self.policy_path = os.path.join(self.data_dir, "optimal_policy.txt")
+
+        with open(self.rewards_path, "a") as f:
             f.write(f"training,epoch,total_reward\n")
 
-        with open("optimal_policy.txt", "a") as f:
+        with open(self.policy_path, "a") as f:
             f.write(f"training,low_recharge,low_search,low_wait,high_search,high_wait\n")
-        
 
     def run_epoch(self, epoch_index) -> None:
         total_reward = 0
@@ -27,7 +32,7 @@ class Recycling:
             self.robot.update_state(new_state)
             total_reward += reward
         self.robot.update_policy(total_reward) # Robot receives a list of dictionaries {'action': action, 'reward': reward}
-        with open("rewards.txt", "a") as f:
+        with open(self.rewards_path, "a") as f:
             f.write(f"{self.training_counter},{epoch_index},{total_reward}\n")
     
     def run_training(self, epochs):
@@ -37,7 +42,7 @@ class Recycling:
     def run_multiple_training(self, num_train, epochs):
         for _ in track(range(num_train), description='Running Multiple Training'):
             self.run_training(epochs)
-            with open("optimal_policy.txt", "a") as f:
+            with open(self.policy_path, "a") as f:
                 f.write(f"{self.training_counter},\
                         {self.robot.q_table[RobotStates.HIGH[LowActions.RECHARGE]]},\
                         {self.robot.q_table[RobotStates.HIGH[LowActions.SEARCH]]},\
